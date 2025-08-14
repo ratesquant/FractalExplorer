7e3339e7b73796f48a62fdb5677b407ffc297c71
Alex Chirokov <alex.chirokov@icloud.com>
2025-08-14 18:17:19 -0400

    Initial Commit

---
+++ FractalExplorer/FractalExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractalExplorerApp.swift
+//  FractalExplorer
+//
+//  Created by Alex Chirokov on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FractalExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
